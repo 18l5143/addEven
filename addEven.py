a754a6964b4c7e04f417919049d5b32b6ac1ac7c
Zusakhe <47633001+18l5143@users.noreply.github.com>
2023-11-08 17:24:35 +0200

    Add files via upload

---
+++ addEven.py
@@ -0,0 +1,13 @@
+
+        
+list1 = [4,64,36,97,23,90,12,5,7]
+    
+def addEven():
+    sum0 = 0 
+    for i in list1:
+         if i%2 ==0:
+              sum0= sum0+i
+    print(sum0)
+    
+    
+addEven()
